4656d3092d02ca6570e738722330967272ccbbb2
admin <you@example.com>
2025-09-09 14:02:38 +0530

    done

--- src/Polymorphism/Rectangle.java
+++ src/Polymorphism/Rectangle.java
@@ -6,15 +6,19 @@ public class Rectangle extends Shape{
     private int width;
 
     public int getLength(){
+
         return this.length;
     }
     public void setLength(int length){
+
         this.length=length;
     }
     public int getWidth(){
+
         return this.width ;
     }
     public void setWidth(int width){
+
         this.width=width;
     }
     @Override
